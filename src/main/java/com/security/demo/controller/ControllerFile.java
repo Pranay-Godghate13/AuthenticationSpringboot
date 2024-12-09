e21da623d948fdb5a91ef21b626bf57be0e0631e
Pranay <pranaygodghate13@gmail.com>
2024-12-09 23:57:56 +0530

    Role based authorization implemented

--- src/main/java/com/security/demo/controller/ControllerFile.java
+++ src/main/java/com/security/demo/controller/ControllerFile.java
@@ -1,6 +1,7 @@
 package com.security.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,5 +12,17 @@ public class ControllerFile {
     public String printHello()
     {
         return "Hello";
+    }
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint()
+    {
+        return "Hello User!!";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint()
+    {
+        return "Hello Admin!!";
     }
 }
--- src/main/java/com/security/demo/controller/SecurityConfig.java
+++ src/main/java/com/security/demo/controller/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.security.demo.controller;
 
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception{
